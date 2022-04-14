a8b5f73b21f0c97cc00ac63b3e5823f2b89b3bd2
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-14 23:58:24 +0500

    Create terraform main file

--- terraform scripts/main.tf
+++ terraform scripts/main.tf
@@ -1,8 +1,8 @@
 module "azure_vm" {
-  source               = "../azure modules/vm"
-  resource_group_name  = local.rg
-  vnet_name            = "az-ss-vn-uks-dev-ch-01"
-  subnet_name          = "az-ss-sn-uks-dev-ch-01"
-  address_space        = ["10.2.0.0/16"]
-  private_subnet_cidrs = ["10.2.0.0/24", "10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24", "10.2.4.0/24", "10.2.5.0/24", "10.2.6.0/24", "10.2.7.0/24"]
+  source        = "../azure modules/azure vm"
+  rgname        = local.rg
+  vmname        = "az-ss-vn-uks-dev-ch-01"
+  location      = "az-ss-sn-uks-dev-ch-01"
+  adminuser     = ""
+  adminpassword = ""
 }
