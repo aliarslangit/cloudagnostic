7803e54ee9cebad27b080f40658360b4601dd801
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-15 12:38:58 +0500

    Update main.tf

--- azure modules/azure vm/main.tf
+++ azure modules/azure vm/main.tf
@@ -13,67 +13,67 @@ resource "azurerm_virtual_network" "main" {
   count               = "1"
 }
 
-# resource "azurerm_subnet" "internal" {
-#   name                 = "internal"
-#   resource_group_name  = azurerm_resource_group.main.name
-#   virtual_network_name = azurerm_virtual_network.main.name
-#   address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
 
-# }
+}
 
-# resource "azurerm_public_ip" "main" {
-#   name                = "${var.vmname}-ip"
-#   location            = azurerm_resource_group.main.location
-#   resource_group_name = azurerm_resource_group.main.name
-#   allocation_method   = "Static"
+resource "azurerm_public_ip" "main" {
+  name                = "${var.vmname}-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
 
-# }
+}
 
-# resource "azurerm_network_interface" "main" {
-#   name                = "${var.vmname}-nic"
-#   location            = azurerm_resource_group.main.location
-#   resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vmname}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-#   ip_configuration {
-#     name                          = "ipconfig"
-#     subnet_id                     = azurerm_subnet.internal.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.main.id
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
 
 
-#   }
+  }
 
-# }
-# resource "azurerm_virtual_machine" "main" {
-#   name                             = "${var.vmname}-vm"
-#   location                         = azurerm_resource_group.main.location
-#   resource_group_name              = azurerm_resource_group.main.name
-#   network_interface_ids            = [azurerm_network_interface.main.id]
-#   vm_size                          = var.vmsize
-#   delete_os_disk_on_termination    = true #to preserve os-disk after deletion, set this poperty to false
-#   delete_data_disks_on_termination = true #to preserve data-disk after deletion, set this poperty to false
+}
+resource "azurerm_virtual_machine" "main" {
+  name                             = "${var.vmname}-vm"
+  location                         = azurerm_resource_group.main.location
+  resource_group_name              = azurerm_resource_group.main.name
+  network_interface_ids            = [azurerm_network_interface.main.id]
+  vm_size                          = var.vmsize
+  delete_os_disk_on_termination    = true #to preserve os-disk after deletion, set this poperty to false
+  delete_data_disks_on_termination = true #to preserve data-disk after deletion, set this poperty to false
 
-#   #Windows or marketplace images can be used as per the requirements
-#   storage_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "16.04-LTS"
-#     version   = "latest"
-#   }
+  #Windows or marketplace images can be used as per the requirements
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 
-#   storage_os_disk {
-#     name              = "${var.vmname}-osdisk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#   }
-#   os_profile {
-#     computer_name  = "hostname"
-#     admin_username = var.adminuser
-#     admin_password = var.adminpassword
-#   }
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
+  storage_os_disk {
+    name              = "${var.vmname}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.adminuser
+    admin_password = var.adminpassword
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 
-# }
+}
