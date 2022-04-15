24423cc62e8b31a55c5c01849d3172b137163cdf
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-15 12:40:23 +0500

    Update main.tf

--- azure modules/azure vm/main.tf
+++ azure modules/azure vm/main.tf
@@ -8,37 +8,37 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
   name                = "${var.vmname}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main[count.index].location
-  resource_group_name = azurerm_resource_group.main[count.index].name
-  count               = "1"
+  location            = azurerm_resource_group.main[count.index][count.index].location
+  resource_group_name = azurerm_resource_group.main[count.index][count.index].name
+  count               = "0"
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.main[count.index][count.index].name
+  virtual_network_name = azurerm_virtual_network.main[count.index].name
   address_prefixes     = ["10.0.2.0/24"]
 
 }
 
 resource "azurerm_public_ip" "main" {
   name                = "${var.vmname}-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main[count.index].location
+  resource_group_name = azurerm_resource_group.main[count.index].name
   allocation_method   = "Static"
 
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.vmname}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main[count.index].location
+  resource_group_name = azurerm_resource_group.main[count.index].name
 
   ip_configuration {
     name                          = "ipconfig"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
+    public_ip_address_id          = azurerm_public_ip.main[count.index].id
 
 
   }
@@ -46,9 +46,9 @@ resource "azurerm_network_interface" "main" {
 }
 resource "azurerm_virtual_machine" "main" {
   name                             = "${var.vmname}-vm"
-  location                         = azurerm_resource_group.main.location
-  resource_group_name              = azurerm_resource_group.main.name
-  network_interface_ids            = [azurerm_network_interface.main.id]
+  location                         = azurerm_resource_group.main[count.index].location
+  resource_group_name              = azurerm_resource_group.main[count.index].name
+  network_interface_ids            = [azurerm_network_interface.main[count.index].id]
   vm_size                          = var.vmsize
   delete_os_disk_on_termination    = true #to preserve os-disk after deletion, set this poperty to false
   delete_data_disks_on_termination = true #to preserve data-disk after deletion, set this poperty to false
