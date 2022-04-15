3aac87bf14ec4fa2e6e78d0f632c95e39f1078d9
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-15 17:31:42 +0500

    Update main.tf

--- azure modules/azure vm/main.tf
+++ azure modules/azure vm/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
   name                = "${var.vmname}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.*.location
-  resource_group_name = azurerm_resource_group.main.*.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 # resource "azurerm_subnet" "internal" {
