285498784c905c5c61706ac0229d6124ff0a2168
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-16 13:17:25 +0500

    Update main.tf

--- gcp modules/main.tf
+++ gcp modules/main.tf
@@ -3,39 +3,34 @@ provider "google" {
   access_token = ""
   project      = "systems-cloud"
 }
-resource "google_compute_network" "vpc_network1" {
-  name = "terraform-network1"
+resource "google_compute_network" "vpc_network" {
+  name = "demo-network"
 }
-# resource "google_compute_instance" "default" {
-#   name         = "test"
-#   machine_type = "e2-medium"
-#   zone         = "asia-east1"
+resource "google_compute_instance" "default" {
+  name         = "demo-vm"
+  machine_type = "e2-medium"
+  zone         = "asia-east1"
 
-#   tags = ["foo", "bar"]
+  tags = ["env", "dev"]
 
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-9"
-#     }
-#   }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
-#   // Local SSD disk
-#   scratch_disk {
-#     interface = "SCSI"
-#   }
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
 
-#   network_interface {
-#     network = "default"
+  network_interface {
+    network = "default"
 
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 
-#   metadata = {
-#     foo = "bar"
-#   }
 
-#   metadata_startup_script = "echo hi > /test.txt"
-
-# }
+}
