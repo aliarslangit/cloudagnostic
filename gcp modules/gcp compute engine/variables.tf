28fe9a69a6576e15fb80a095a077a703d28f3e9c
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-19 13:55:50 +0500

    Update variables.tf

--- gcp modules/gcp compute engine/variables.tf
+++ gcp modules/gcp compute engine/variables.tf
@@ -7,7 +7,7 @@ variable "vmname" {
 
 variable "zone" {
   type    = string
-  default = "e2-highmem-4"
+  default = "us-central1-a"
 }
 //------------------------------------VM Configurations------------------------------------\\
 # variable "adminuser" {
@@ -20,7 +20,7 @@ variable "zone" {
 # }
 variable "machine_type" {
   type    = string
-  default = "e2_medium"
+  default = "c2-standard-4"
 }
 variable "cloud" {
   default = "azure"
