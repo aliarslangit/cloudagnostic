c0840fcf9e33f348671ca1ef0a175962d0aa8185
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-19 12:54:37 +0500

    Update main.tf

--- terraform scripts/main.tf
+++ terraform scripts/main.tf
@@ -8,10 +8,10 @@ module "azure_vm" {
   source        = "../azure modules/azure vm"
   rgname        = "rg-vm"
   vmname        = "demo-vm-01"
-  location      = "east us"
+  location      = "West US"
   adminuser     = "vmuser"
   adminpassword = "Adm!nistr@tor"
-  vmsize        = "Standard_D2"
+  vmsize        = "Standard_F2"
   cloud         = local.cloud
 }
 module "gcp_vm" {
