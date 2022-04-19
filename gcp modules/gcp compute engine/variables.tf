94c2dd885ce153d4e7758a6324cfd0463340b482
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-19 12:09:43 +0500

    Update variables.tf

--- gcp modules/gcp compute engine/variables.tf
+++ gcp modules/gcp compute engine/variables.tf
@@ -1,9 +1,5 @@
 
 //------------------------------------General Properties------------------------------------\\
-variable "rgname" {
-  type    = string
-  default = "RG-git-DEMO-LINUXVM"
-}
 variable "vmname" {
   type    = string
   default = "demo-vm"
@@ -14,14 +10,14 @@ variable "zone" {
   default = "asia-east1"
 }
 //------------------------------------VM Configurations------------------------------------\\
-variable "adminuser" {
-  type    = string
-  default = "vmuser"
-}
-variable "adminpassword" {
-  type    = string
-  default = "P@ssw0rd!234"
-}
+# variable "adminuser" {
+#   type    = string
+#   default = "vmuser"
+# }
+# variable "adminpassword" {
+#   type    = string
+#   default = "P@ssw0rd!234"
+# }
 variable "machine_type" {
   type    = string
   default = "e2_medium"
