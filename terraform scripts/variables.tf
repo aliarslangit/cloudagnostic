3d4e8729d572192e1fc78b6bc9c739bd8e7e8fa8
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-04-26 11:54:53 +0500

    Update variables.tf

--- terraform scripts/variables.tf
+++ terraform scripts/variables.tf
@@ -1,3 +1,12 @@
 variable "cloud" {
   default = "azure"
 }
+variable "vmsize" {
+  default = "Standard_D2"
+}
+variable "adminuser" {
+  default = "user"
+}
+variable "adminpassword" {
+  default = "P@ssw0rd!"
+}
