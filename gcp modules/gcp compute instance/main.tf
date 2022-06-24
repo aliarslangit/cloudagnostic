75c465ef345a8d90edb305ff3b8038cae924db82
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-06-24 13:50:59 +0500

    Create main.tf

---
+++ gcp modules/gcp compute instance/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  credentials = file("gcp-creds.json")
+  project     = "systems-cloud"
+}
+resource "google_compute_network" "vpc_network1" {
+  name = "terraform-network1"
+}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "asia-east1"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+}
