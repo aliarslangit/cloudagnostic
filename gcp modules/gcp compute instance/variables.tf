2deecbd205c5a0f847d3e69edd422cbff80e1605
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-06-27 11:21:20 +0500

    Create variables.tf

---
+++ gcp modules/gcp compute instance/variables.tf
@@ -0,0 +1,13 @@
+variable "machine_type" {
+  type=string
+  default = "value"
+}
+
+variable "vm_name" {
+  type = string
+  default = "value"
+}
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
