140dfde687a9743aede26c4aaa2b1c91cc50a3ed
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-07-15 13:15:23 +0500

    Update variables.tf

--- azure modules/Azure APIM/variables.tf
+++ azure modules/Azure APIM/variables.tf
@@ -1,28 +1,4 @@
-//-------------------------------Service principal------------------------------//
 
-variable "azure_subscription_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_client_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_client_secret" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_tenant_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
 
 //------------------------CONFIGIRATIONS------------------------------------------//
 
@@ -48,8 +24,3 @@ variable "units" {
   default = "1"
 }
 
-//------------------------TAGS------------------------------------------//
-variable "environment"{
-  type = string
-  default = "Dev"
-}
