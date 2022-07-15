9aba99b087a39b81f5261bad0b63efabb9303ee2
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-07-15 13:15:54 +0500

    Update variables.tf

--- azure modules/Azure LoadBalancer/variables.tf
+++ azure modules/Azure LoadBalancer/variables.tf
@@ -1,28 +1,4 @@
-//-------------------------------Service principal------------------------------//
 
-variable "azure_subscription_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_client_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_client_secret" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "azure_tenant_id" {
-  type        = string
-  default     = ""
-  description = "description"
-}
 //------------------------General Properties------------------------------------------//
 variable "lbname" {
   type    = string
