fce6e4266766762ea77f783c44150887de84a36b
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-07-15 13:16:09 +0500

    Update provider.tf

--- azure modules/azure vm/provider.tf
+++ azure modules/azure vm/provider.tf
@@ -4,13 +4,7 @@ provider "azurerm" {
 
   version = "=2.30.0"
 
-  subscription_id = var.azure_subscription_id
-
-  client_id = var.azure_client_id
-
-  client_secret = var.azure_client_secret
-
-  tenant_id = var.azure_tenant_id
+ 
 
   features {}
 
