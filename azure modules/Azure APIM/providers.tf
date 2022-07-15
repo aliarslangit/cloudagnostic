cf31b3789b3e70166e8a03f1eb9032db02402833
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-07-15 13:15:30 +0500

    Update providers.tf

--- azure modules/Azure APIM/providers.tf
+++ azure modules/Azure APIM/providers.tf
@@ -1,20 +1,10 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.69.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-features {}
 
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+
+  version = "=2.30.0"
+
+
+  features {}
+
 }
